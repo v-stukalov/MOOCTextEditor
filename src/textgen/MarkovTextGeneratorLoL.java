33d855449160af6c99c77e1fc2f021d67f02415f
Vitalii Stukalov <vitaliy.stukalov@gmail.com>
2015-11-28 13:19:10 +0200

    week 3

--- src/textgen/MarkovTextGeneratorLoL.java
+++ src/textgen/MarkovTextGeneratorLoL.java
@@ -1,162 +1,209 @@
 package textgen;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Random;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/** 
+/**
  * An implementation of the MTG interface that uses a list of lists.
- * @author UC San Diego Intermediate Programming MOOC team 
+ *
+ * @author UC San Diego Intermediate Programming MOOC team
  */
 public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
-	// The list of words with their next words
-	private List<ListNode> wordList; 
-	
-	// The starting "word"
-	private String starter;
-	
-	// The random number generator
-	private Random rnGenerator;
-	
-	public MarkovTextGeneratorLoL(Random generator)
-	{
-		wordList = new LinkedList<ListNode>();
-		starter = "";
-		rnGenerator = generator;
-	}
-	
-	
-	/** Train the generator by adding the sourceText */
-	@Override
-	public void train(String sourceText)
-	{
-		// TODO: Implement this method
-	}
-	
-	/** 
-	 * Generate the number of words requested.
-	 */
-	@Override
-	public String generateText(int numWords) {
-	    // TODO: Implement this method
-		return null;
-	}
-	
-	
-	// Can be helpful for debugging
-	@Override
-	public String toString()
-	{
-		String toReturn = "";
-		for (ListNode n : wordList)
-		{
-			toReturn += n.toString();
-		}
-		return toReturn;
-	}
-	
-	/** Retrain the generator from scratch on the source text */
-	@Override
-	public void retrain(String sourceText)
-	{
-		// TODO: Implement this method.
-	}
-	
-	// TODO: Add any private helper methods you need here.
-	
-	
-	/**
-	 * This is a minimal set of tests.  Note that it can be difficult
-	 * to test methods/classes with randomized behavior.   
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		// feed the generator a fixed random value for repeatable behavior
-		MarkovTextGeneratorLoL gen = new MarkovTextGeneratorLoL(new Random(42));
-		String textString = "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
-		System.out.println(textString);
-		gen.train(textString);
-		System.out.println(gen);
-		System.out.println(gen.generateText(20));
-		String textString2 = "You say yes, I say no, "+
-				"You say stop, and I say go, go, go, "+
-				"Oh no. You say goodbye and I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"I say high, you say low, "+
-				"You say why, and I say I don't know. "+
-				"Oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"Why, why, why, why, why, why, "+
-				"Do you say goodbye. "+
-				"Oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"You say yes, I say no, "+
-				"You say stop and I say go, go, go. "+
-				"Oh, oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello,";
-		System.out.println(textString2);
-		gen.retrain(textString2);
-		System.out.println(gen);
-		System.out.println(gen.generateText(20));
-	}
+    // The list of words with their next words
+    private List<ListNode> wordList;
+
+    // The starting "word"
+    private String starter;
+
+    //private String prevWord;
+
+    // The random number generator
+    private Random rnGenerator;
+
+    public MarkovTextGeneratorLoL(Random generator) {
+        wordList = new LinkedList<>();
+        starter = "";
+        rnGenerator = generator;
+    }
+
+
+    /**
+     * Train the generator by adding the sourceText
+     */
+    @Override
+    public void train(String sourceText) {
+        List<String> words = getTokens(sourceText, "[a-zA-Z,.!?]+");
+        if (words.isEmpty()) return;
+        starter = words.get(0);
+        int i = 1;
+        while (i < words.size()) {
+            putInPlace(words.get(i - 1), words.get(i++));
+        }
+        putInPlace(words.get(i - 1), starter);
+    }
+
+
+    private void putInPlace(String prevWord, String nextWord) {
+        ListNode node = findListNode(prevWord);
+        if (node != null) {
+            node.addNextWord(nextWord);
+        } else {
+            ListNode newNode = new ListNode(prevWord);
+            newNode.addNextWord(nextWord);
+            wordList.add(newNode);
+        }
+    }
+
+    private ListNode findListNode(String word) {
+        for (ListNode node : wordList) {
+            if (word.equals(node.getWord())) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    protected List<String> getTokens(String text, String pattern) {
+        ArrayList<String> tokens = new ArrayList<String>();
+        Pattern tokSplitter = Pattern.compile(pattern);
+        Matcher m = tokSplitter.matcher(text);
+        while (m.find()) {
+            tokens.add(m.group());
+        }
+        return tokens;
+    }
+
+    /**
+     * Generate the number of words requested.
+     */
+    @Override
+    public String generateText(int numWords) {
+        if (numWords < 1 || wordList.isEmpty()) return "";
+        String currWord = starter;
+        StringBuilder output = new StringBuilder(currWord);
+        while (--numWords > 0) {
+            ListNode node = findListNode(currWord);
+            currWord = node.getRandomNextWord(rnGenerator);
+            output.append(" ").append(currWord);
+        }
+        return output.toString();
+    }
+
+
+    // Can be helpful for debugging
+    @Override
+    public String toString() {
+        String toReturn = "";
+        for (ListNode n : wordList) {
+            toReturn += n.toString();
+        }
+        return toReturn;
+    }
+
+    /**
+     * Retrain the generator from scratch on the source text
+     */
+    @Override
+    public void retrain(String sourceText) {
+        wordList = new LinkedList<>();
+        starter = "";
+        train(sourceText);
+    }
+
+    // TODO: Add any private helper methods you need here.
+
+
+    /**
+     * This is a minimal set of tests.  Note that it can be difficult
+     * to test methods/classes with randomized behavior.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // feed the generator a fixed random value for repeatable behavior
+        MarkovTextGeneratorLoL gen = new MarkovTextGeneratorLoL(new Random(42));
+        String textString = "hi there! hi. Leo, hi hi hi Leo. Leo hi there.";
+        System.out.println(textString);
+        gen.train(textString);
+        System.out.println(gen);
+        System.out.println(gen.generateText(0));
+
+        textString = "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
+        System.out.println(textString);
+        gen.train(textString);
+        System.out.println(gen);
+        System.out.println(gen.generateText(20));
+        String textString2 = "You say yes, I say no, " +
+                "You say stop, and I say go, go, go, " +
+                "Oh no. You say goodbye and I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello. " +
+                "I say high, you say low, " +
+                "You say why, and I say I don't know. " +
+                "Oh no. " +
+                "You say goodbye and I say hello, hello, hello. " +
+                "I don't know why you say goodbye, I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello. " +
+                "Why, why, why, why, why, why, " +
+                "Do you say goodbye. " +
+                "Oh no. " +
+                "You say goodbye and I say hello, hello, hello. " +
+                "I don't know why you say goodbye, I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello. " +
+                "You say yes, I say no, " +
+                "You say stop and I say go, go, go. " +
+                "Oh, oh no. " +
+                "You say goodbye and I say hello, hello, hello. " +
+                "I don't know why you say goodbye, I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello, hello, hello, " +
+                "I don't know why you say goodbye, I say hello, hello, hello,";
+        System.out.println(textString2);
+        gen.retrain(textString2);
+        System.out.println(gen);
+        System.out.println(gen.generateText(20));
+    }
 
 }
 
-/** Links a word to the next words in the list 
- * You should use this class in your implementation. */
-class ListNode
-{
+/**
+ * Links a word to the next words in the list
+ * You should use this class in your implementation.
+ */
+class ListNode {
     // The word that is linking to the next words
-	private String word;
-	
-	// The next words that could follow it
-	private List<String> nextWords;
-	
-	ListNode(String word)
-	{
-		this.word = word;
-		nextWords = new LinkedList<String>();
-	}
-	
-	public String getWord()
-	{
-		return word;
-	}
+    private String word;
 
-	public void addNextWord(String nextWord)
-	{
-		nextWords.add(nextWord);
-	}
-	
-	public String getRandomNextWord(Random generator)
-	{
-		// TODO: Implement this method
-	    // The random number generator should be passed from 
-	    // the MarkovTextGeneratorLoL class
-	    return null;
-	}
+    // The next words that could follow it
+    private List<String> nextWords;
 
-	public String toString()
-	{
-		String toReturn = word + ": ";
-		for (String s : nextWords) {
-			toReturn += s + "->";
-		}
-		toReturn += "\n";
-		return toReturn;
-	}
-	
+    ListNode(String word) {
+        this.word = word;
+        nextWords = new LinkedList<String>();
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void addNextWord(String nextWord) {
+        nextWords.add(nextWord);
+    }
+
+    public String getRandomNextWord(Random generator) {
+        return nextWords.get(generator.nextInt(nextWords.size()));
+    }
+
+    public String toString() {
+        String toReturn = word + ": ";
+        for (String s : nextWords) {
+            toReturn += s + "->";
+        }
+        toReturn += "\n";
+        return toReturn;
+    }
+
 }
 
 
--- src/textgen/MyLinkedListTester.java
+++ src/textgen/MyLinkedListTester.java
@@ -1,162 +1,285 @@
 /**
- * 
+ *
  */
 package textgen;
-
-import static org.junit.Assert.*;
-
-import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 /**
  * @author UC San Diego MOOC team
- *
  */
 public class MyLinkedListTester {
 
-	private static final int LONG_LIST_LENGTH =10; 
+    private static final int LONG_LIST_LENGTH = 10;
 
-	MyLinkedList<String> shortList;
-	MyLinkedList<Integer> emptyList;
-	MyLinkedList<Integer> longerList;
-	MyLinkedList<Integer> list1;
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		// Feel free to use these lists, or add your own
-	    shortList = new MyLinkedList<String>();
-		shortList.add("A");
-		shortList.add("B");
-		emptyList = new MyLinkedList<Integer>();
-		longerList = new MyLinkedList<Integer>();
-		for (int i = 0; i < LONG_LIST_LENGTH; i++)
-		{
-			longerList.add(i);
-		}
-		list1 = new MyLinkedList<Integer>();
-		list1.add(65);
-		list1.add(21);
-		list1.add(42);
-		
-	}
+    MyLinkedList<String> shortList;
+    MyLinkedList<Integer> emptyList;
+    MyLinkedList<Integer> longerList;
+    MyLinkedList<Integer> list1;
 
-	
-	/** Test if the get method is working correctly.
-	 */
-	/*You should not need to add much to this method.
-	 * We provide it as an example of a thorough test. */
-	@Test
-	public void testGet()
-	{
-		//test empty list, get should throw an exception
-		try {
-			emptyList.get(0);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-			
-		}
-		
-		// test short list, first contents, then out of bounds
-		assertEquals("Check first", "A", shortList.get(0));
-		assertEquals("Check second", "B", shortList.get(1));
-		
-		try {
-			shortList.get(-1);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		try {
-			shortList.get(2);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		// test longer list contents
-		for(int i = 0; i<LONG_LIST_LENGTH; i++ ) {
-			assertEquals("Check "+i+ " element", (Integer)i, longerList.get(i));
-		}
-		
-		// test off the end of the longer array
-		try {
-			longerList.get(-1);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		try {
-			longerList.get(LONG_LIST_LENGTH);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		}
-		
-	}
-	
-	
-	/** Test removing an element from the list.
-	 * We've included the example from the concept challenge.
-	 * You will want to add more tests.  */
-	@Test
-	public void testRemove()
-	{
-		int a = list1.remove(0);
-		assertEquals("Remove: check a is correct ", 65, a);
-		assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
-		assertEquals("Remove: check size is correct ", 2, list1.size());
-		
-		// TODO: Add more tests here
-	}
-	
-	/** Test adding an element into the end of the list, specifically
-	 *  public boolean add(E element)
-	 * */
-	@Test
-	public void testAddEnd()
-	{
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        // Feel free to use these lists, or add your own
+        shortList = new MyLinkedList<String>();
+        shortList.add("A");
+        shortList.add("B");
+        emptyList = new MyLinkedList<Integer>();
+        longerList = new MyLinkedList<Integer>();
+        for (int i = 0; i < LONG_LIST_LENGTH; i++) {
+            longerList.add(i);
+        }
+        list1 = new MyLinkedList<Integer>();
+        list1.add(65);
+        list1.add(21);
+        list1.add(42);
+
+    }
+
+
+    /**
+     * Test if the get method is working correctly.
+     */
+    /*You should not need to add much to this method.
+     * We provide it as an example of a thorough test. */
+    @Test
+    public void testGet() {
+        //test empty list, get should throw an exception
+        try {
+            emptyList.get(0);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+
+        // test short list, first contents, then out of bounds
+        assertEquals("Check first", "A", shortList.get(0));
+        assertEquals("Check second", "B", shortList.get(1));
+
+        try {
+            shortList.get(-1);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            shortList.get(2);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        // test longer list contents
+        for (int i = 0; i < LONG_LIST_LENGTH; i++) {
+            assertEquals("Check " + i + " element", (Integer) i, longerList.get(i));
+        }
+
+        // test off the end of the longer array
+        try {
+            longerList.get(-1);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            longerList.get(LONG_LIST_LENGTH);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+    }
+
+
+    /**
+     * Test removing an element from the list.
+     * We've included the example from the concept challenge.
+     * You will want to add more tests.
+     */
+    @Test
+    public void testRemove() {
+        int a = list1.remove(0);
+        assertEquals("Remove: check a is correct ", 65, a);
+        assertEquals("Remove: check element 0 is correct ", (Integer) 21, list1.get(0));
+        assertEquals("Remove: check size is correct ", 2, list1.size());
+        try {
+            emptyList.remove(0);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+
+        for (int i = 1; i < LONG_LIST_LENGTH; i++) {
+            longerList.remove(1);
+        }
+        assertEquals("Remove: check size is correct ", 1, longerList.size());
+        assertEquals("Remove: check element 0 is correct ", (Integer) 0, longerList.get(0));
+        longerList.remove(0);
+        assertEquals("Remove: check size is correct ", 0, longerList.size());
+        // TODO: Add more tests here
+    }
+
+    /**
+     * Test adding an element into the end of the list, specifically
+     * public boolean add(E element)
+     */
+    @Test
+    public void testAddEnd() {
         // TODO: implement this test
-		
-	}
+        emptyList.add(11); //size=1
+        assertEquals(1, emptyList.size);
+        emptyList.add(12); //size=2
+        assertEquals(2, emptyList.size);
+        emptyList.add(13); //size=3
+        assertEquals(3, emptyList.size);
+        emptyList.set(2, 14); //size=3
+        assertEquals(3, emptyList.size);
+        emptyList.remove(1); //size=2
+        assertEquals(2, emptyList.size);
+//        assertEquals("forward: 11->14->\n" +
+//                "backward: 14->11->", emptyList.toString());
+    }
 
-	
-	/** Test the size of the list */
-	@Test
-	public void testSize()
-	{
-		// TODO: implement this test
-	}
 
-	
-	
-	/** Test adding an element into the list at a specified index,
-	 * specifically:
-	 * public void add(int index, E element)
-	 * */
-	@Test
-	public void testAddAtIndex()
-	{
+    /**
+     * Test the size of the list
+     */
+    @Test
+    public void testSize() {
         // TODO: implement this test
-		
-	}
-	
-	/** Test setting an element in the list */
-	@Test
-	public void testSet()
-	{
-	    // TODO: implement this test
-	    
-	}
-	
-	
-	// TODO: Optionally add more test methods.
-	
+        emptyList.add(0, 11); //size=1
+        emptyList.add(12); //size=2
+        emptyList.add(0, 13); //size=3
+        emptyList.set(2, 14); //size=3
+        emptyList.remove(1); //size=2
+        assertEquals(2, emptyList.size());
+    }
+
+
+    /**
+     * Test adding an element into the list at a specified index,
+     * specifically:
+     * public void add(int index, E element)
+     */
+    @Test
+    public void testAddAtIndex() {
+        // TODO: implement this test
+        //test empty list, get should throw an exception
+        emptyList.add(0, 11);
+        assertEquals("Check first", (Integer) 11, emptyList.get(0));
+        assertEquals("Check size", 1, emptyList.size);
+
+        // test short list, first contents, then out of bounds
+        shortList.add(0, "E");
+        shortList.add(1, "F");
+        System.out.println(shortList);
+
+        assertEquals("Check first", "E", shortList.get(0));
+        assertEquals("Check second", "F", shortList.get(1));
+        assertEquals("Check third", "A", shortList.get(2));
+        assertEquals("Check fourth", "B", shortList.get(3));
+        assertEquals("Check size", 4, shortList.size);
+
+        try {
+            shortList.get(4);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            shortList.add(2, null);
+            fail("Null element");
+        } catch (NullPointerException e) {
+
+        }
+        // test longer list contents
+        for (int i = 0; i < LONG_LIST_LENGTH; i++) {
+            longerList.add(i * 2, i);
+        }
+
+        for (int i = 0, j = 0; i < 2 * LONG_LIST_LENGTH; i++, j = i / 2) {
+            assertEquals("Check " + i + " element", (Integer) j, longerList.get(i));
+        }
+
+        // test off the end of the longer array
+        try {
+            longerList.get(-1);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            longerList.get(2 * LONG_LIST_LENGTH);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+
+    }
+
+    /**
+     * Test setting an element in the list
+     */
+    @Test
+    public void testSet() {
+        // TODO: implement this test
+        try {
+            emptyList.set(1, 11);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+
+        // test short list, first contents, then out of bounds
+        assertEquals("Insert first", "A", shortList.set(0, "E"));
+        assertEquals("Insert second", "B", shortList.set(1, "F"));
+
+        try {
+            shortList.get(-1);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            shortList.get(2);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            shortList.set(1, null);
+            fail("Null element");
+        } catch (NullPointerException e) {
+
+        }
+        // test longer list contents
+        for (int i = 0; i < LONG_LIST_LENGTH; i++) {
+            assertEquals("Check " + i + " element", (Integer) i, longerList.set(i, i * 2));
+        }
+        assertEquals("Check size", LONG_LIST_LENGTH, longerList.size());
+        for (int i = 0; i < LONG_LIST_LENGTH; i++) {
+            assertEquals("Check " + i + " element", (Integer) (i * 2), longerList.get(i));
+        }
+        // test off the end of the longer array
+        try {
+            longerList.get(-1);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        try {
+            longerList.get(LONG_LIST_LENGTH);
+            fail("Check out of bounds");
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+    }
+
+
+    // TODO: Optionally add more test methods.
+
 }
